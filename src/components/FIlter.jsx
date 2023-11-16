ea1fff32b270c2d36de0c93bc0b7c9706b4cc936
CapriceSeum <capriceseum@gmail.com>
2023-11-16 16:01:25 +0100

    initialize v1

--- src/components/FIlter.jsx
+++ src/components/FIlter.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import '../css/filter.css'
 
 
-
 export default function Filter() {
 
   const [fountainDrinkData, setFountainDrinkData] = useState([]);
